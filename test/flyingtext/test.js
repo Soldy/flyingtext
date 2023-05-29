b6c76dc963e2c582004986bb14a1187e42358faf
Soldy <4786022+Soldy@users.noreply.github.com>
2023-05-29 20:53:39 +0100

    resize example
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- test/flyingtext/test.js
+++ test/flyingtext/test.js
@@ -1,6 +1,10 @@
 const $lorem = new loremBase();
 
-window.onload = ()=>{(new FlyingTextClass(
-    document.getElementsByClassName('holder')[0],
-    $lorem.paragraph()
-)).run();}
+window.onload = ()=>{
+    const flyingText = new FlyingTextClass(
+        document.getElementsByClassName('holder')[0],
+        $lorem.paragraph()
+    );
+    flyingText.run();
+    window.onresize = flyingText.resize;
+};
